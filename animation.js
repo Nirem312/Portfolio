90a2bd5f9dc066c572718e019d644710bbe0bb70
Glitch (colorful-spangled-zydeco) <none>
2023-04-22 15:14:33 +0000

    💎🕌 Checkpoint ./animation.js:45835844/181 ./english.html:45835844/25 ./index.html:45835844/1812 ./stylish.css:45835844/23

--- animation.js
+++
@@ -1,7 +0,0 @@
-$("left-to-right-square").hide();
-
-$(document).ready(function(){
-    $("#left-to-right-square").click(function(){
-      $("#left-to-right-square").slideToggle("slow");
-    });
-  });
