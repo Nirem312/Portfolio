dc83fd45e900a1bcee20768802d3f2dd2fd0c640
Glitch (colorful-spangled-zydeco) <none>
2023-06-05 23:25:55 -0300

    First commit

---
+++ animation.js
@@ -0,0 +1,16 @@
+var myIndex = 0;
+console.log('fuck you')
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var y = document.getElementsByClassName("mySlides2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+    y[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";
+  y[myIndex-1].style.display = "block"; 
+  setTimeout(carousel, 3500);
+}
