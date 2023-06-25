ed890e0287244b82c7e927d0b400c6c76b9b880c
Nirem312 <95861480+Nirem312@users.noreply.github.com>
2023-06-25 01:45:21 -0300

    Adds new project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,83 +9,79 @@
 </script>
 
 <template>
-    <section class="main-text">
-      <h1>
-        Nicolas Remigio
-      </h1>
-      <p>
-        Frontend developer focusing on the React and Vue frameworks. <br>
-      </p>
-    </section>
-    
-    <section class="estudios"> 
-      <h2>
-        Studies
-      </h2>
-      <p>
-        <b>High school</b>: Instituto Tecnológico Superior “Arias-Balparda” (2016 - 2019) <br>
-        <b>College</b>: Facultad de Ingeniería UdelaR (2019 - Present)
-      </p>
-    </section>
-    
-    <section class="aligned-right habilidades">
-      <h2>
-        Technologies and skills
-      </h2>
-      <p>
-        <b>Advanced</b>: Python, JavaScript, CSS, HTML.<br>
-        <b>Intermediate</b>: VueJS, ReactJS.<br>
-        <b>Basic</b>: Java, C++, VisualBasic.NET, Node.JS.<br>
-        <b>DBMS</b>: MySQL, MongoDB.<br>
-        <b>Operating systems</b>: Windows, Linux.<br>
-        <b>Networking</b>: Devices, protocols, topologies.<br>
-        <b>Pen-testing</b>: Labs (HackTheBox, picoCTF, PortSwigger), virtualization.<br>
-      </p>
-    </section> 
-    
-    <section class="experiencia"> 
-      <h2>
-        Work experience
-      </h2>
-      <p>
-        <b>Tata Consultancy Services</b>: <br>
-        Service desk specialist (March 7<sup>th</sup> 2022 - December 20<sup>th</sup> 2022).
-        Subject Matter Expert (December 20<sup>th</sup> 2022 - Present).
-      </p>
-    </section>
-    
-    <section class="aligned-right lenguajes">
-      <h2>
-        Languages
-      </h2>
-      <p>
-        <b>Spanish</b>: Native. <br>
-        <b>English</b>: C.E.R.F.L.: C1, C.A.E. Cambridge certificate.
-      </p>
-    </section>
-    
-    <section class="cursos">
-      <h2>
-        Courses and certifications
-      </h2>
-      <p class="cursos">
-          <a href="https://www.freecodecamp.org/learn" target="_blank"><b>freeCodeCamp:</b></a><br>
-          <a href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/" target="_blank">-JavaScript Algorithms and Data Structures</a><br>
-          <a href="https://www.freecodecamp.org/learn/front-end-development-libraries/" target="_blank">-Front End Development Libraries</a><br>
-          <a href="https://www.freecodecamp.org/learn/data-visualization/" target="_blank">-Data visualization</a><br>
-          <a href="https://scrimba.com/" target="_blank"><b>Scrimba:</b></a><br>
-          <a href="https://scrimba.com/learn/learnreact/" target="_blank">-Learn React</a><br>
-          <a href="https://www.linkedin.com/learning/" target="_blank"><b>LinkedIn learning:</b></a><br>
-          <a href="https://www.linkedin.com/learning/paths/become-a-full-stack-web-developer?" target="_blank">-<b>Learning path: Become a full stack web developer</b></a><br>
-          <a href="https://www.linkedin.com/learning/paths/networking-and-administration-fundamentals?" target="_blank">-<b>Learning path: Networking and Administration Fundamentals</b></a><br>
-          <a href="https://www.linkedin.com/learning/paths/improve-your-cisco-network-security-skills?" target="_blank">-<b>Learning path: Improve Your Cisco Network Security Skills</b></a><br>
-          <a href="https://www.linkedin.com/learning/linux-system-engineer-web-servers-and-dns-using-apache-nginx-and-bind?" target="_blank">-Linux System Engineer: Web Servers and DNS Using Apache, NGINX, and BIND</a><br>
-      </p>
-    </section>
+  <section class="main-text">
+    <h1>
+      Nicolas Remigio
+    </h1>
+    <p>
+      Frontend developer focusing on the React and Vue frameworks. <br>
+    </p>
+  </section>
+  
+  <section class="estudios"> 
+    <h2>
+      Studies
+    </h2>
+    <p>
+      <b>High school</b>: Instituto Tecnológico Superior “Arias-Balparda” (2016 - 2019) <br>
+      <b>College</b>: Facultad de Ingeniería UdelaR (2019 - Present)
+    </p>
+  </section>
+  
+  <section class="aligned-right habilidades">
+    <h2>
+      Technologies and skills
+    </h2>
+    <p>
+      <b>Advanced</b>: Python, JavaScript, CSS, HTML.<br>
+      <b>Intermediate</b>: VueJS, ReactJS.<br>
+      <b>Basic</b>: Java, C++, VisualBasic.NET, Node.JS.<br>
+      <b>DBMS</b>: MySQL, MongoDB.<br>
+      <b>Operating systems</b>: Windows, Linux.<br>
+      <b>Networking</b>: Devices, protocols, topologies.<br>
+      <b>Pen-testing</b>: Labs (HackTheBox, picoCTF, PortSwigger), virtualization.<br>
+    </p>
+  </section> 
+  
+  <section class="experiencia"> 
+    <h2>
+      Work experience
+    </h2>
+    <p>
+      <b>Tata Consultancy Services</b>: <br>
+      Service desk specialist (March 7<sup>th</sup> 2022 - December 20<sup>th</sup> 2022).
+      Subject Matter Expert (December 20<sup>th</sup> 2022 - Present).
+    </p>
+  </section>
+  
+  <section class="aligned-right lenguajes">
+    <h2>
+      Languages
+    </h2>
+    <p>
+      <b>Spanish</b>: Native. <br>
+      <b>English</b>: C.E.R.F.L.: C1, C.A.E. Cambridge certificate.
+    </p>
+  </section>
+  
+  <section class="cursos">
+    <h2>
+      Courses and certifications
+    </h2>
+    <p class="cursos">
+      <a href="https://www.freecodecamp.org/learn" target="_blank"><b>freeCodeCamp:</b></a><br>
+      <a href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/" target="_blank">-JavaScript Algorithms and Data Structures</a><br>
+      <a href="https://www.freecodecamp.org/learn/front-end-development-libraries/" target="_blank">-Front End Development Libraries</a><br>
+      <a href="https://www.freecodecamp.org/learn/data-visualization/" target="_blank">-Data visualization</a><br>
+      <a href="https://scrimba.com/" target="_blank"><b>Scrimba:</b></a><br>
+      <a href="https://scrimba.com/learn/learnreact/" target="_blank">-Learn React</a><br>
+      <a href="https://www.linkedin.com/learning/" target="_blank"><b>LinkedIn learning:</b></a><br>
+      <a href="https://www.linkedin.com/learning/paths/become-a-full-stack-web-developer?" target="_blank">-<b>Learning path: Become a full stack web developer</b></a><br>
+      <a href="https://www.linkedin.com/learning/paths/networking-and-administration-fundamentals?" target="_blank">-<b>Learning path: Networking and Administration Fundamentals</b></a><br>
+      <a href="https://www.linkedin.com/learning/paths/improve-your-cisco-network-security-skills?" target="_blank">-<b>Learning path: Improve Your Cisco Network Security Skills</b></a><br>
+      <a href="https://www.linkedin.com/learning/linux-system-engineer-web-servers-and-dns-using-apache-nginx-and-bind?" target="_blank">-Linux System Engineer: Web Servers and DNS Using Apache, NGINX, and BIND</a><br>
+    </p>
+  </section>
 
-    <Footer />
+  <Footer />
 </template>
-
-<style>
-
-</style>
